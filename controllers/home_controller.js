313b3ea4ed273e932059fcadc2febf68bc52eb17
Aman <aman@MacBook-Air.local>
2020-09-22 08:01:32 +0530

    Creating Flash Message

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,37 +1,33 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req, res){
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
-
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts:  posts
-    //     });
-    // });
-
-    // populate the user of each post
-    Post.find({}).populate('user').
-    populate({
-        path:'comments',
-            populate:{
-                path:'user'
+module.exports.home = async function(req, res){
+      
+    try{
+        let posts = await Post.find({})
+        //populate each user 
+        .populate('user')
+        .populate({
+            path:'comments',
+                populate:{
+                    path:'user'
+                }
             }
-        }
-    )
-    .exec(function(err, posts){
-        User.find({},function(errr,users){
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts:  posts,
-                all_users:users
-            });
-        });
+        )
 
-    })
+        let users = await User.find({});
+        
+            return res.render('home', {
+               title: "Codeial | Home",
+               posts:  posts,
+               all_users:users
+            });
 
 }
+catch(err){
+    console.log('Error',err);
+    return;
+}
 
+}
 // module.exports.actionName = function(req, res){}
